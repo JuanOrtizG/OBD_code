66a4ec1b56ceb89506a8d5fb67df85a74dfd0fbe
Juan <juanrodrigortiz@gmail.com>
2020-11-24 17:13:17 -0300

    Probando alzar a github

--- EX_CAN.C
+++ EX_CAN.C
@@ -1,14 +1,14 @@
 /*
 @autor: Juan Ortiz
-@versión: 
+@version: 
 @fecha:
-@Tema: Protocolo de Comunicación CAN
+@Tema: Protocolo de Comunicacion CAN
 */
 
 #include "18F4580.h"
 #fuses HS,NOPROTECT,NOLVP,NOWDT
 #use delay(clock=16000000)
-//#use delay(oscillator=20Mhz, clock = 80Mhz)
+
 #use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7)
 
 #DEFINE CAN_DO_DEBUG          FALSE
@@ -22,53 +22,190 @@
 
 
 #include "can-18F4580.c"
-
-/*
-int16 ms;
-
-#int_timer2
-void isr_timer2(void) {
-   ms++; //keep a running timer that increments every milli-second
-}
-*/
+#include "obdpic.h"
 
 
 //int1 can_getd(int32 & id, int * data, int & len, struct rx_stat & stat)
-void obd_get()
+int32 obd_get(int pid)
 {
    struct rx_stat rxstat;
    int32 rx_id;
    int in_data[8];
    int rx_len;
-   int i;
+   //int i;
    
    if ( can_kbhit() )   //if data is waiting in buffer...
-      {
-         if(can_getd(rx_id, &in_data[0], rx_len, rxstat)) { //...then get data from buffer
-           // printf("\r\nGOT: BUFF=%U ID=%LU LEN=%U OVF=%U ", rxstat.buffer, rx_id, rx_len, rxstat.err_ovfl);
-           // printf("FILT=%U RTR=%U EXT=%U INV=%U", rxstat.filthit, rxstat.rtr, rxstat.ext, rxstat.inv);
-            printf("\r\n    RECIBIENDO DATA = ");
-            for ( i=0;i<rx_len;i++) {
-               printf("%X ",in_data[i]);
-            }
-            printf("\r\n");
-         }
-         else {
-            printf("\r\nFAIL on GETD\r\n");
-         }
-
-      }
+   {
+     can_getd(rx_id, &in_data[0], rx_len, rxstat);
+      
+   /*PARA VERIFICAR LOS PID*/
+     #define A in_data[3]
+     #define B in_data[4]
+     #define C in_data[5]
+     #define D in_data[6]
+     #define uint32_t unsigned int32
+     #define int16_t int16
+     //uint8_t value[4];
+   
+   switch (pid) {
+       default:
+       case PIDS_SUPPORT_01_20: // raw
+       case MONITOR_STATUS_SINCE_DTCS_CLEARED: // raw
+       case FREEZE_DTC: // raw
+       case PIDS_SUPPORT_21_40: // raw
+       case PIDS_SUPPORT_41_60: // raw
+       case MONITOR_STATUS_THIS_DRIVE_CYCLE: // raw
+         // NOTE: return value can lose precision!
+         return ((uint32_t)A << 24 | (uint32_t)B << 16 | (uint32_t)C << 8 | (uint32_t)D);
+   
+       case FUEL_SYSTEM_STATUS: // raw
+       case RUN_TIME_SINCE_ENGINE_START:
+       case DISTANCE_TRAVELED_WITH_MIL_ON:
+       case DISTANCE_TRAVELED_SINCE_CODES_CLEARED:
+       case TIME_RUN_WITH_MIL_ON:
+       case TIME_SINCE_TROUBLE_CODES_CLEARED:
+         return (A * 256.0 + B);
+   
+       case CALCULATED_ENGINE_LOAD:
+       case THROTTLE_POSITION:
+       case COMMANDED_EGR:
+       case COMMANDED_EVAPORATIVE_PURGE:
+       case FUEL_TANK_LEVEL_INPUT:
+       case RELATIVE_THROTTLE_POSITION:
+       case ABSOLUTE_THROTTLE_POSITION_B:
+       case ABSOLUTE_THROTTLE_POSITION_C:
+       case ABSOLUTE_THROTTLE_POSITION_D:
+       case ABSOLUTE_THROTTLE_POSITION_E:
+       case ABSOLUTE_THROTTLE_POSITION_F:
+       case COMMANDED_THROTTLE_ACTUATOR:
+       case ETHANOL_FUEL_PERCENTAGE:
+       case RELATIVE_ACCELERATOR_PEDAL_POSITTION:
+       case HYBRID_BATTERY_PACK_REMAINING_LIFE:
+         return (A / 2.55);
+   
+       case COMMANDED_SECONDARY_AIR_STATUS: // raw
+       case OBD_STANDARDS_THIS_VEHICLE_CONFORMS_TO: // raw
+       case OXYGEN_SENSORS_PRESENT_IN_2_BANKS: // raw
+       case OXYGEN_SENSORS_PRESENT_IN_4_BANKS: // raw
+       case AUXILIARY_INPUT_STATUS: // raw
+       case FUEL_TYPE: // raw
+       case EMISSION_REQUIREMENT_TO_WHICH_VEHICLE_IS_DESIGNED: // raw
+         return (A);
+   
+       case OXYGEN_SENSOR_1_SHORT_TERM_FUEL_TRIM:
+       case OXYGEN_SENSOR_2_SHORT_TERM_FUEL_TRIM:
+       case OXYGEN_SENSOR_3_SHORT_TERM_FUEL_TRIM:
+       case OXYGEN_SENSOR_4_SHORT_TERM_FUEL_TRIM:
+       case OXYGEN_SENSOR_5_SHORT_TERM_FUEL_TRIM:
+       case OXYGEN_SENSOR_6_SHORT_TERM_FUEL_TRIM:
+       case OXYGEN_SENSOR_7_SHORT_TERM_FUEL_TRIM:
+       case OXYGEN_SENSOR_8_SHORT_TERM_FUEL_TRIM:
+         return ((B / 1.28) - 100.0);
+         break;
+   
+       case ENGINE_COOLANT_TEMPERATURE:
+       case AIR_INTAKE_TEMPERATURE:
+       case AMBIENT_AIR_TEMPERATURE:
+       case ENGINE_OIL_TEMPERATURE:
+         return (A - 40.0);
+   
+       case SHORT_TERM_FUEL_TRIM_BANK_1:
+       case LONG_TERM_FUEL_TRIM_BANK_1:
+       case SHORT_TERM_FUEL_TRIM_BANK_2:
+       case LONG_TERM_FUEL_TRIM_BANK_2:
+       case EGR_ERROR:
+         return ((A / 1.28) - 100.0);
+   
+       case FUEL_PRESSURE:
+         return (A * 3.0);
+   
+       case INTAKE_MANIFOLD_ABSOLUTE_PRESSURE:
+       case VEHICLE_SPEED:
+       case WARM_UPS_SINCE_CODES_CLEARED:
+       case ABSOLULTE_BAROMETRIC_PRESSURE:
+         return (A);
+   
+       case ENGINE_RPM:
+         return ((A * 256.0 + B) / 4.0);
+   
+       case TIMING_ADVANCE:
+         return ((A / 2.0) - 64.0);
+   
+       case MAF_AIR_FLOW_RATE:
+         return ((A * 256.0 + B) / 100.0);
+   
+       case FUEL_RAIL_PRESSURE:
+         return ((A * 256.0 + B) * 0.079);
+   
+       case FUEL_RAIL_GAUGE_PRESSURE:
+       case FUEL_RAIL_ABSOLUTE_PRESSURE:
+         return ((A * 256.0 + B) * 10.0);
+   
+       case OXYGEN_SENSOR_1_FUEL_AIR_EQUIVALENCE_RATIO:
+       case OXYGEN_SENSOR_2_FUEL_AIR_EQUIVALENCE_RATIO:
+       case OXYGEN_SENSOR_3_FUEL_AIR_EQUIVALENCE_RATIO:
+       case OXYGEN_SENSOR_4_FUEL_AIR_EQUIVALENCE_RATIO:
+       case OXYGEN_SENSOR_5_FUEL_AIR_EQUIVALENCE_RATIO:
+       case OXYGEN_SENSOR_6_FUEL_AIR_EQUIVALENCE_RATIO:
+       case OXYGEN_SENSOR_7_FUEL_AIR_EQUIVALENCE_RATIO:
+       case OXYGEN_SENSOR_8_FUEL_AIR_EQUIVALENCE_RATIO:
+       case 0x34:
+       case 0x35:
+       case 0x36:
+       case 0x37:
+       case 0x38:
+       case 0x39:
+       case 0x3a:
+       case 0x3b:
+         return (((A * 256.0 + B) * 2.0) / 65536.0);
+   
+       case EVAP_SYSTEM_VAPOR_PRESSURE:
+         return (((int16_t)(A * 256.0 + B)) / 4.0);
+   
+       case CATALYST_TEMPERATURE_BANK_1_SENSOR_1:
+       case CATALYST_TEMPERATURE_BANK_2_SENSOR_1:
+       case CATALYST_TEMPERATURE_BANK_1_SENSOR_2:
+       case CATALYST_TEMPERATURE_BANK_2_SENSOR_2:
+         return (((A * 256.0 + B) / 10.0) - 40.0);
+   
+       case CONTROL_MODULE_VOLTAGE:
+         return ((A * 256.0 + B) / 1000.0);
+   
+       case ABSOLUTE_LOAD_VALUE:
+         return ((A * 256.0 + B) / 2.55);
+   
+       case FUEL_AIR_COMMANDED_EQUIVALENCE_RATE:
+         return (2.0 * (A * 256.0 + B) / 65536.0);
+   
+       case ABSOLUTE_EVAP_SYSTEM_VAPOR_PRESSURE:
+         return ((A * 256.0 + B) / 200.0);
+   
+       case 0x54:
+         return ((A * 256.0 + B) - 32767.0);
+   
+       case FUEL_INJECTION_TIMING:
+         return (((A * 256.0 + B) / 128.0) - 210.0);
+   
+       case ENGINE_FUEL_RATE:
+         return ((A * 256.0 + B) / 20.0);
+     }
+     
+      }//FIN DEL IF PARA KBHIT
 }
+      
+      
 
 
 //void obd_put(int time)
-void obd_put()
+void obd_put(int id, int pid, int1 trama)
 {
-   //send a request (tx_rtr=1) for 8 bytes of data (tx_len=8) from id 24 (tx_id=24)
-   int out_data[8]={1,2,3,4,5,5,5,5};
-   int32 tx_id=12;
+   
+   int out_data[8]={2,1,0,0,0,0,0,0};
+   out_data[2]=pid;
+   
+   int32 tx_id=id;
    int1 tx_rtr=0;
-   int1 tx_ext=0;
+   int1 tx_ext=trama;
    int tx_len=8;
    int tx_pri=3;
 
@@ -80,40 +217,49 @@ void obd_put()
       {
          //ms=time;
          i=can_putd(tx_id, out_data, tx_len,tx_pri,tx_ext,tx_rtr); //put data on transmit buffer
-         if (i != 0xFF) { //success, a transmit buffer was open
-            // printf("\r\nPUT %U: ID=%LU LEN=%U ", i, tx_id, tx_len);
-            // printf("PRI=%U EXT=%U RTR=%U\r\n   DATA = ", tx_pri, tx_ext, tx_rtr);
-            printf("\r\n    ENVIANDO DATA = ");
-            for (i=0;i<tx_len;i++) {
-               printf("%X ",out_data[i]);
-            }
-            printf("\r\n");
+         if (i != 0xFF) 
+         {
+           ;
          }
-         else { //fail, no transmit buffer was open
-            printf("\r\nFAIL on PUTD\r\n");
+         else { 
+            printf("\r\n Fallo de env�o de la trama... \r\n");
          }
       }
 }
 void main() {
    
-   printf("\r\n\r\nCCS CAN EXAMPLE\r\n");
-
-   //setup_timer_2(T2_DIV_BY_4,79,16);   //setup up timer2 to interrupt every 1ms if using 20Mhz clock
-
    can_init();
    enable_interrupts(INT_TIMER2);   //enable timer2 interrupt
    enable_interrupts(GLOBAL);       //enable all interrupts (else timer2 wont happen)
 
-   printf("\r\nRunning...");
+   printf("\n Running... \n");
 
    while(TRUE)
    {
-      printf ("ciclo.. \n");
-      obd_get();
+      
+      //void obd_put(int id, int pid, int1 trama)
+      obd_put(ID_QUERY,VEHICLE_SPEED,TRAMA_ESTANDAR);
       delay_ms(500);
-      obd_put();
+      printf ("\n  VEHICLE_SPEED: %ld KM/h ->  ",obd_get(VEHICLE_SPEED)) ;
+      delay_ms(50);
+       
+      
+      obd_put(ID_QUERY,ENGINE_RPM,TRAMA_ESTANDAR);
       delay_ms(500);
+      printf ("ENGINE_RPM: %ld RPM.",obd_get(ENGINE_RPM)) ;
+      delay_ms(50);
+      
+      
+      obd_put(ID_QUERY,ENGINE_FUEL_RATE,TRAMA_ESTANDAR);
+      delay_ms(500);
+      printf ("FUEL_RATE: %ld LxKM...",obd_get(ENGINE_FUEL_RATE)) ;
+      delay_ms(50);
+      
+     
+      
+      
    
+      
       
    }
 }
